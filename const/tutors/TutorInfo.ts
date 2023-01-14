f14730d625c88b63995ead3459688c19a10f8b55
Vikum-AI <63595074+Vikum-AI@users.noreply.github.com>
2023-01-15 01:51:56 +0530

    Tutor info data

---
+++ const/tutors/TutorInfo.ts
@@ -0,0 +1,22 @@
+import ITutorIntro from "../../widgets/tutor-intro/ITutorIntro";
+
+const TutorInfo: ITutorIntro[] = [
+  {
+    tutorId: 1,
+    tutorName: "Malith Withanaarachchi",
+    description:
+      "Mr. Malith Withanaarachchi, a senior physics teachers at a leading international school in Colombo, which is one of the largest international schools delivering both Cambridge and Edexcel curriculums for over 5500 students. He is a graduate from University of Sri Jayewardenepura, Sri Lanka specialized in Physics, Mathematics and Computer sciences. Mr Withanaarachchi completed his masters degree in physics education from University of Colombo.",
+    phoneNumber: "+94 713 114 565 / +94 772 783 139",
+    email: "ceo@physicsstudios.com",
+    image: "/img/profile-pic-1.png",
+  },
+  {
+    tutorId: 2,
+    tutorName: "Buddhika Uduwaraarachchi",
+    description:
+      "Advanced Level Physics classes conducted by an advanced level physics teacher at Lyceum Wattala, who has completed an honors degree in Physics with experience in teaching the subject for more than 6 years. Also working as a Subject matter expert in physics at Candela Software technologies. Highly results oriented and the lesson plan will be prepared for each and every student specifically and it will be communicated with the parent.  In the class, Past paper questions, Model papers, and additional problems will be provided and discussed. Well organized and systematic teaching techniques will be used with proven results in previous years.  Results Guaranteed.",
+    image: "/img/buddhika-pfp.png",
+  },
+];
+
+export default TutorInfo;
