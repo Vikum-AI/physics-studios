a9aeac8ebce480c80f29731077db8d66e6244540
Vikum-AI <63595074+Vikum-AI@users.noreply.github.com>
2023-01-15 01:50:58 +0530

    Header component refactor

--- components/3dheader/Header.tsx
+++ components/3dheader/Header.tsx
@@ -3,11 +3,11 @@ import IHeader from "./IHeader";
 
 export default function Header({ label, extras }: IHeader) {
   return (
-    <div className={`${extras}`}>
-      <h1 className="text-7xl font-bold m-6 text-gray-200">{label}</h1>
-      <h1 className="text-7xl font-bold m-6 text-gray-700 relative -top-20 left-5">
+    <div className={`relative w-full h-20 mb-20 ${extras}`}>
+      <h3 className="text-7xl font-bold mx-6 text-gray-200">{label}</h3>
+      <h3 className="text-7xl font-bold m-6 text-gray-700 absolute -top-2 sm:left-5">
         {label}
-      </h1>
+      </h3>
     </div>
   );
 }
